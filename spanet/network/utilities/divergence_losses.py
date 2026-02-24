a27da0d68622446b030b7a7a3de669805762c0eb
tcoulvert <63161166+tcoulvert@users.noreply.github.com>
2026-02-24 12:15:32 -0800

    Update divergence_losses.py

--- spanet/network/utilities/divergence_losses.py
+++ spanet/network/utilities/divergence_losses.py
@@ -23,9 +23,11 @@ def assignment_cross_entropy_loss(prediction: Tensor, target_data: Tensor, targe
     ravel_target = (target_data * ravel_sizes).sum(1)
     ravel_prediction = prediction.reshape(batch_size, -1).contiguous()
 
+    # Hotfix to mask out bad events that pass mask
+    target_mask = torch.logical_and(target_mask, ~torch.isinf(log_probability))
+    
     log_probability = ravel_prediction.gather(-1, ravel_target.view(-1, 1)).squeeze()
     log_probability = log_probability.masked_fill(~target_mask, 0.0)
-    log_probability = log_probability.masked_fill(torch.isinf(log_probability), 0.0)
 
     focal_scale = (1 - torch.exp(log_probability)) ** gamma
 
